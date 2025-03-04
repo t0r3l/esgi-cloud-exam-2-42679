853a69f93bba7df0c9e8aa1f292bc1a9e35201d0
t0r3l <tom.daniel.andre@gmail.com>
2025-03-04 22:05:10 +0100

    db

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  ProcessingInstruction.env.DB_URL,
+  process.env.DB_URL,
   {
     dialectOptions: {
       ssl: {
